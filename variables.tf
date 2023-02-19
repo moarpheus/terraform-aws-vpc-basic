b44216b2d6fae1bc29d2ad3ea232970197835a80
Juris Gribuska <juris.gribuska@itv.com>
2023-02-19 15:47:18 +0000

    exposed more variables to expand functionality

--- variables.tf
+++ variables.tf
@@ -21,3 +21,21 @@ variable "enable_dns_support" {
   type        = bool
   default     = true
 }
+
+variable "public_subnets_cidr" {
+  description = "Public Subnets CIDR"
+  type = list
+  default = ["10.0.0.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type = list
+  default = ["eu-west-2a", "us-west-2b"]
+}
+
+variable "private_subnets_cidr" {
+  description = "Private subnets CIDR"
+  type = list
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
