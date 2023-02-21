8193169d7f38c9927f2dbe97071c0cd86d2c7289
Juris Gribuska <juris.gribuska@itv.com>
2023-02-21 07:48:31 +0000

    feat() Added environment to use in tags

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
   tags = {
-    Name        = "${var.vpc_name}"
+    Name        = "${var.environment}-vpc"
+    Environment = "${var.environment}"
   }
 }
 
@@ -27,6 +28,11 @@ resource "aws_security_group" "default" {
     protocol  = "-1"
     self      = "true"
   }
+
+  tags = {
+    Name        = "${var.environment}-default-sg"
+    Environment = "${var.environment}"
+  }
 }
 
 # Availability zones
@@ -41,12 +47,22 @@ resource "aws_subnet" "private_subnet" {
   cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.environment}-${element(var.private_subnets_cidr, count.index)}-subnet"
+    Environment = "${var.environment}"
+  }
 }
 
 # Elastic IP for NAT
 resource "aws_eip" "nat_eip" {
   vpc        = true
   depends_on = [aws_internet_gateway.ig]
+
+  tags = {
+    Name        = "${var.environment}-nat-eip"
+    Environment = "${var.environment}"
+  }
 }
 
 # NAT to allow private subnets to commnunicate outside VPC
@@ -54,6 +70,11 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = "${aws_eip.nat_eip.id}"
   subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
   depends_on    = [aws_internet_gateway.ig]
+
+  tags = {
+    Name        = "${var.environment}-nat"
+    Environment = "${var.environment}"
+  }
 }
 
 # Public subnets
@@ -63,21 +84,41 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-${element(var.public_subnets_cidr, count.index)}-subnet"
+    Environment = "${var.environment}"
+  }
 }
 
 # Internet gateway for the public subnet
 resource "aws_internet_gateway" "ig" {
   vpc_id = "${aws_vpc.this.id}"
+
+  tags = {
+    Name        = "${var.environment}-ig"
+    Environment = "${var.environment}"
+  }
 }
 
 # Routing table for private subnet
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.this.id}"
+
+  tags = {
+    Name        = "${var.environment}-private-route"
+    Environment = "${var.environment}"
+  }
 }
 
 # Routing table for public subnet
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.this.id}"
+
+  tags = {
+    Name        = "${var.environment}-public-route"
+    Environment = "${var.environment}"
+  }
 }
 
 resource "aws_route" "public_internet_gateway" {
@@ -85,6 +126,7 @@ resource "aws_route" "public_internet_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.ig.id}"
 }
+
 resource "aws_route" "private_nat_gateway" {
   route_table_id         = "${aws_route_table.private.id}"
   destination_cidr_block = "0.0.0.0/0"
